04b9d54fcbb8e4f12b4ffc36ebd0a9d7864e7f6e
zydreaa <zydreaa@gmail.com>
2022-04-20 19:17:06 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+
+        //Sets is used for storing unique items
+        //it can't take induplicates
+
+        //Items created inside a collection are transformed into objects
+    }
+}
